7079c231f221dfc4af283a1191a8bf8ae02e0d93
mrkwon <erased11@gmail.com>
2019-12-29 21:23:27 +0900

    feat: View 에 필요한 기능을 추가한다.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -10,12 +10,24 @@ import java.util.List;
 public class Application {
     // TODO 구현 진행
     public static void main(String[] args) {
+        OutputView.printApplicationMenu();
+
+        final int selectedApplicationMenu = InputView.inputApplicationMenu();
+
+        // selectedApplicationMenu, 1, 2 일 때
+
         final List<Table> tables = TableRepository.tables();
         OutputView.printTables(tables);
 
         final int tableNumber = InputView.inputTableNumber();
 
+        // selectedApplicationMenu, 1 일 때
+
         final List<Menu> menus = MenuRepository.menus();
         OutputView.printMenus(menus);
+
+        final int selectedOrderMenu = InputView.inputOrderMenu();
+
+        final int orderMenuCount = InputView.inputOrderMenuCount();
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,4 +9,19 @@ public class InputView {
         System.out.println("## 주문할 테이블을 선택하세요.");
         return scanner.nextInt();
     }
+
+    public static int inputApplicationMenu() {
+        System.out.println("원하는 기능을 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputOrderMenu() {
+        System.out.println("## 등록할 메뉴를 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputOrderMenuCount() {
+        System.out.println("## 메뉴의 수량을 입력하세요.");
+        return scanner.nextInt();
+    }
 }
