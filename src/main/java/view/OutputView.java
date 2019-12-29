79ed91102486a0cdbf2209dc9632d890610650e4
mrkwon <erased11@gmail.com>
2019-12-29 21:30:03 +0900

    feat: OutputView 에 Table 을 표시하는 기능을 추가한다.

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -8,7 +8,9 @@ import java.util.List;
 public class OutputView {
     private static final String TOP_LINE = "┌ ─ ┐";
     private static final String TABLE_FORMAT = "| %s |";
-    private static final String BOTTOM_LINE = "└ ─ ┘";
+    private static final String BOTTOM_LINE = "└ %s ┘";
+    private static final String OCCUPIED_MARK = "₩";
+    private static final String NOT_OCCUPIED_MARK = "─";
 
     public static void printApplicationMenu() {
         System.out.println("## 메인화면");
@@ -21,9 +23,9 @@ public class OutputView {
     public static void printTables(final List<Table> tables) {
         System.out.println("## 테이블 목록");
         final int size = tables.size();
-        printLine(TOP_LINE, size);
+        printTopLine(size);
         printTableNumbers(tables);
-        printLine(BOTTOM_LINE, size);
+        printBottomLine(tables);
     }
 
     public static void printMenus(final List<Menu> menus) {
@@ -32,9 +34,9 @@ public class OutputView {
         }
     }
 
-    private static void printLine(final String line, final int count) {
+    private static void printTopLine(final int count) {
         for (int index = 0; index < count; index++) {
-            System.out.print(line);
+            System.out.print(OutputView.TOP_LINE);
         }
         System.out.println();
     }
@@ -44,5 +46,19 @@ public class OutputView {
             System.out.printf(TABLE_FORMAT, table);
         }
         System.out.println();
+    }
+
+    private static void printBottomLine(final List<Table> tables) {
+        for (final Table table : tables) {
+            System.out.printf(BOTTOM_LINE, printOccupied(table));
+        }
+        System.out.println();
+    }
+
+    private static String printOccupied (Table table) {
+        if (table.isOccupied()) {
+            return OCCUPIED_MARK;
+        }
+        return NOT_OCCUPIED_MARK;
     }
 }
