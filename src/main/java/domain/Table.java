f96905124001a4060ec93b24c18993a0f40195b1
mrkwon <erased11@gmail.com>
2019-12-29 21:10:54 +0900

    test: Table 에 menu 와 menu 수를 가지고 있는 orders 필드를 추가한다.
    - orderMenu 주문을 추가하는 메서드
    - getMenuCount 주문 수를 get 하는 메서드

--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -2,18 +2,22 @@ package domain;
 
 import domain.exception.AlreadyOccupiedTableException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Table {
     private final int number;
     private boolean occupied;
+    private Map<Menu, Integer> orders;
 
     public Table(final int number) {
         this.number = number;
-        this.occupied = false;
+        initTable();
     }
 
-    @Override
-    public String toString() {
-        return Integer.toString(number);
+    public void initTable() {
+        this.occupied = false;
+        this.orders = new HashMap<>();
     }
 
     public boolean isOccupied() {
@@ -25,5 +29,26 @@ public class Table {
             throw new AlreadyOccupiedTableException("이미 손님이 있는 테이블 입니다.");
         }
         this.occupied = true;
+    }
+
+    public Integer orderMenu(final Menu menu, final Integer count) {
+        if (orders.containsKey(menu)) {
+            return countAfterPut(menu, count + orders.get(menu));
+        }
+        return countAfterPut(menu, count);
+    }
+
+    private Integer countAfterPut(final Menu menu, final Integer count) {
+        orders.put(menu, count);
+        return orders.get(menu);
+    }
+
+    public Integer getMenuCount(final Menu menu) {
+        return orders.get(menu);
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(number);
     }
 }
