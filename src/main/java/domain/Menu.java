90c130984643e9254b51cbf4f5f0c98064fe124b
mrkwon <erased11@gmail.com>
2019-12-29 21:05:59 +0900

    test: Menu number 로 비교하는 메서드를 추가한다.

--- src/main/java/domain/Menu.java
+++ src/main/java/domain/Menu.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 public class Menu {
     private final int number;
     private final String name;
@@ -13,8 +15,28 @@ public class Menu {
         this.price = price;
     }
 
+    public boolean isSameNumber(int number) {
+        return this.number == number;
+    }
+
     @Override
     public String toString() {
         return category + " " + number + " - " + name + " : " + price + "원";
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Menu menu = (Menu) o;
+        return number == menu.number &&
+                price == menu.price &&
+                Objects.equals(name, menu.name) &&
+                category == menu.category;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, name, category, price);
     }
 }
